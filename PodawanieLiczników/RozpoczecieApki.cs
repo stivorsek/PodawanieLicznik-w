9d9b7aaf1f65031b64567e49cbd936e730d6a74b
Sigmorv2 <sigmorek@gmail.com>
2023-04-06 17:01:30 +0200

    RozpoczecieApki

---
+++ PodawanieLiczników/RozpoczecieApki.cs
@@ -0,0 +1,34 @@
+﻿using PodawanieLiczników;
+using System.ComponentModel;
+
+Console.WriteLine("Witaj w programie do przeliczania opłat za wodę, gaz oraz prąd");
+
+var occupant = new Occupant("test", "test");
+while (true)
+{
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine("Proszę wybrać za co chcesz poznac opłatę");
+    Console.WriteLine("1)Opłata za gaz");
+    Console.WriteLine("2)Opłata za wodę");
+    Console.WriteLine("3)Opłata za prąd");
+    Console.WriteLine("Lub wybierz 'q' aby wyjść z programu");
+    var media = Console.ReadLine();
+    if (media == "q")
+    {
+        break;
+    }
+    try
+    {
+        occupant.ChoseMedia(media);
+    }
+    catch (Exception e) 
+    { 
+        Console.WriteLine($"Error:{e.Message}");
+    }
+}
+Console.WriteLine("");
+Console.WriteLine($"Proszę zapłacić {occupant.gasBill} zł za gaz");
+Console.WriteLine($"Proszę zapłacić {occupant.waterBill} zł za wode");
+Console.WriteLine($"Proszę zapłacić {occupant.electricityBill} zł za prad");
+
